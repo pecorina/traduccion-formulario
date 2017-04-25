9f574e36def706e915276cec7dec669fdde49d62
pecorina <constanza.pecori@gmail.com>
2017-04-25 13:13:16 -0300

    trabajo guiado

---
+++ js/main.js
@@ -0,0 +1,16 @@
+function translate (){
+	var title = document.getElementById ("form-signin-heading");
+	var email = document.getElementById ("inputEmail");
+    var password = document.getElementById ("inputPassword");
+    var remember = document.getElementsByTagName ("span")[0];
+    var button = document.getElementsByClassName ("btn")[0];
+
+  title.innerHTML = "Por favor inicia sesion";
+  email.placeholder = "Correo Electronico";
+  password.placeholder = "Contraseña";
+  remember.innerHTML = "Recordar datos";
+  button.innerHTML = "Iniciar Sesión";
+
+}
+
+translate();
